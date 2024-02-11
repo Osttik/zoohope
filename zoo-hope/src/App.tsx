77096ce15b88b6e9aa3655453339fb09549d86a8
Osttik <lololka23@gmail.com>
2024-02-11 16:06:41 +0200

    Add problematic setInterval

--- zoo-hope/src/App.tsx
+++ zoo-hope/src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    setInterval(() => console.log(5), 5000);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
