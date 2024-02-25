47ed7f87e7f1a196f67672f2f8a7b42a5cdffbef
Osttik <lololka23@gmail.com>
2024-02-25 15:14:28 +0200

    Removed unessesary files Add axios

--- zoo-hope/src/App.tsx
+++ zoo-hope/src/App.tsx
@@ -13,6 +13,7 @@ function App() {
 
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
