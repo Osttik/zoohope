86f9655a5026ed98e293a1ddfc1d1eaef6e90421
Bohdan-Fursa <98541426+Bohdan-Fursa@users.noreply.github.com>
2024-02-25 14:41:58 +0200

    in-progres

--- zoo-hope/src/App.tsx
+++ zoo-hope/src/App.tsx
@@ -1,33 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import logo from './images/icons/logo.svg';
-
+import logo from "./images/icons/logo.svg";
+import Donate from "./components/donate";
 function App() {
-  useEffect(() => {
-    const interval = setInterval(() => console.log(5), 5000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Donate />;
 }
 
 export default App;
