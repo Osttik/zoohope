0c0bc4e94b369962d778b48cf6f9296dcc66be20
Bohdan <kovadlobogdan@gmail.com>
2024-03-09 11:46:00 +0200

    footer repetive code moved to another components

--- zoo-hope/src/components/footer/index.tsx
+++ zoo-hope/src/components/footer/index.tsx
@@ -1,8 +1,10 @@
 import "../../styles/index.scss";
 import { elements } from "../../data/footer";
 import LogoNavBar from "../../images/logo/logo-white.png";
-import sendIcon from "../../images/icons/send.svg";
-import NavLink from "react-bootstrap/esm/NavLink";
+import { FooterNavBlockLink } from "../footerNavBlockLink";
+import { FooterBottomText } from "../footerBottomText";
+import { FooterNavContactsBlock } from "../footerNavContactsBlock";
+import { FooterNavBlock } from "../footerNavBlock";
 
 export const Footer = () => {
   return (
@@ -13,75 +15,25 @@ export const Footer = () => {
           <p className="footer__logo-text">zoonadia</p>
         </div>
         <div className="footer__navigation">
-          {elements.map((e) =>
+          {elements.map((e:Object) =>
             e.hasOwnProperty("listInput") ? (
-              <div className="footer__nav-block footer-nav-block footer-nav-block_contacts">
-                <h5 className="footer-nav-block__title">{e.listName}</h5>
-                <ul className="footer-nav-block__list">
-                  <li className="footer-nav-block__item">
-                    <div className="footer-nav-block__social-media">
-                      {e.ISocialMedias?.map((e) => {
-                        return (
-                          <NavLink
-                            className="footer-nav-block__social-media-icon"
-                            href={e.url}
-                          >
-                            <img src={e.src} alt={e.alt} />
-                          </NavLink>
-                        );
-                      })}
-                    </div>
-                  </li>
-                  {e.IListItems?.map((e) => {
-                    return (
-                      <li className="footer-nav-block__item">
-                        <p className="footer-nav-block__text">{e.name}</p>
-                      </li>
-                    );
-                  })}
-                  <div className="footer-nav-block__input-area">
-                    <input
-                      className="footer-nav-block__input"
-                      placeholder={e.listInput}
-                      type="text"
-                    />
-                    <button className="footer-nav-block__button">
-                      <img src={sendIcon} alt="" />
-                    </button>
-                  </div>
-                </ul>
-              </div>
+              <FooterNavContactsBlock props={e}/>
             ) : (
-              <div className="footer__nav-block footer-nav-block">
-                <h5 className="footer-nav-block__title">{e.listName}</h5>
-                <ul className="footer-nav-block__list">
-                  {e.IListItems?.map((e) => {
-                    return (
-                      <li className="footer-nav-block__item">
-                        <NavLink href="#" className="footer-nav-block__link">
-                          {e.name}
-                        </NavLink>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
+              <FooterNavBlock props={e} />
             )
           )}
         </div>
         <div className="footer__bottom footer-bottom">
-          {elements.map((e) => {
+          {elements.map((e:Object) => {
             return e.hasOwnProperty("footerBottomText") ? (
-              <p className="footer-bottom__text">{e.footerBottomText}</p>
+              <FooterBottomText props={e}/>
             ) : (
               <></>
             );
           })}
-          {elements.map((e) => {
+          {elements.map((e:Object) => {
             return e.hasOwnProperty("footerBottomLink") ? (
-              <NavLink href="#" className="footer-bottom__link">
-                {e.footerBottomLink}
-              </NavLink>
+              <FooterNavBlockLink props={e}/>
             ) : (
               <></>
             );
