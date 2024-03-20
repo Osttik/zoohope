52caa8728137a94636f391326ac6f8863e4ced59
SergioVed <vedankinsergij@gmail.com>
2024-03-20 16:31:21 +0200

    removed unnecessary changes from another branch

--- server/navigation/routes/petRoutes.js
+++ server/navigation/routes/petRoutes.js
@@ -4,20 +4,11 @@ const PetModel = require('../../models/Pet');
 module.exports.addPet = async (req, res) => {
     try {
         const newPet = new PetModel(req.body);
-        newPet.validate(async function(err) {
-            if (err) {
-                res.status(400).json({message: err.message})
-            } else {
-                try {
-                    const savedPet = await newPet.save();
-                    res.status(201).json(savedPet);
-                } catch (err) {
-                    res.status(500).json({message: err.message})
-                }
-            }
-        })
-    } catch (err) {
-        res.status(500).json({ message: err.message });
+        const savedPet = await newPet.save();
+
+        res.status(201).json(savedPet);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 };
 
@@ -39,16 +30,12 @@ module.exports.updatePet = async (req, res) => {
         const { id } = req.params;
 
         const updatedPet = await PetModel.findByIdAndUpdate(id, req.body, { new: true });
+
         if (!updatedPet) {
             return res.status(404).json({ message: 'No pet in database' });
         }
-        updatedPet.validate(function(err){
-            if (err) {
-                res.status(400).json({message: err.message})
-            } else {
-                res.json(updatedPet);
-            }
-        })
+
+        res.json(updatedPet);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
