4a09b5a555101442491e2e994b6d6da315d17a6e
Bohdan-Fursa <98541426+Bohdan-Fursa@users.noreply.github.com>
2024-03-31 14:47:15 +0300

    complete_donate

--- zoo-hope/src/App.tsx
+++ zoo-hope/src/App.tsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from "react";
-import logo from "./images/icons/logo.svg";
 import { NavBar } from "./components/navBar";
+import { Footer } from "./components/footer";
 import Donate from "./components/donate";
+import { Pet } from "./components/PetPage/Pet";
+
 function App() {
-  useEffect(() => {
-    const interval = setInterval(() => console.log(5), 5000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
-  return <Donate />;
+  return (
+    <div className="App">
+      <Donate />
+      {/* <Pet /> */}
+    </div>
+  );
 }
 
 export default App;
--- zoo-hope/src/components/donate/index.tsx
+++ zoo-hope/src/components/donate/index.tsx
@@ -1,18 +1,36 @@
+import { useState, ChangeEvent } from "react";
 import "./../../styles/index.scss";
+import Paw from "../../images/icons/paw_icon.png";
 const Donate = () => {
+  const [inputValue, setInputValue] = useState<number>(0);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(parseInt(event.target.value));
+  };
+  const handleButtonClick = (amount: number) => {
+    setInputValue(inputValue + amount);
+  };
   return (
     <div className="wrapper_donate">
       <span>Допомогти нам</span>
       <div className="wrapper_donate__input">
-        <input type="number" inputMode="numeric"></input>
+        <input
+          type="number"
+          inputMode="numeric"
+          value={inputValue}
+          onChange={handleInputChange}
+        ></input>
       </div>
       <div className="wrapper_buttons">
-        <button>+10</button>
-        <button>+100</button>
-        <button>+1000</button>
+        <button onClick={() => handleButtonClick(10)}>+10</button>
+        <button onClick={() => handleButtonClick(100)}>+100</button>
+        <button onClick={() => handleButtonClick(1000)}>+1000</button>
       </div>
       <div className="wrapper_buttons__pay">
-        <button>Сплатити</button>
+        <a href="https://send.monobank.ua/jar/BhQbi8BR8?fbclid=IwAR04yzp55zkm1xeE9D0UD5mYfiPAc7X8mBvWJk2VpCYU1N_uRkOgomVj04E">
+          Сплатити
+          <img src={Paw} />
+        </a>
       </div>
     </div>
   );
