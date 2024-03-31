3807419edf5a37c750497493589a98766349a30e
harukadoo <buriakkdiana@gmail.com>
2024-03-31 15:01:13 +0300

    update

--- zoo-hope/src/App.tsx
+++ zoo-hope/src/App.tsx
@@ -3,12 +3,15 @@ import { NavBar } from './components/navBar';
 import { Footer } from './components/footer';
 import Donate from "./components/donate";
 import { Pet } from './components/PetPage/Pet';
+import { AdminPage } from './components/admin-page';
 
 function App() {
   return (
     <div className="App">
       {/* <Donate /> */}
       {/* <Pet /> */}
+
+      <AdminPage />
     </div>
   );
 }
