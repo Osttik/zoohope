a561252a9e9938f8dba67f8d8b4b4d4734cb49f9
SergioVed <vedankinsergij@gmail.com>
2024-05-19 17:00:06 +0300

    Fixed filtering

--- zoo-hope/src/data/petList.ts
+++ zoo-hope/src/data/petList.ts
@@ -22,6 +22,7 @@ let pageSize = 12;
     {label: "7", value: "7"},
     {label: "8", value: "8"},
     {label: "9", value: "9"},
+    {label: "10", value: "10"},
     {label: "11", value: "11"},
     {label: "12", value: "12"},
     {label: "13", value: "13"},
