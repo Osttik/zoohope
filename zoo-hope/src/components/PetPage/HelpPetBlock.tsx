d06d686f8e3181676984c1906bada8fd042dee52
harukadoo <buriakkdiana@gmail.com>
2024-05-28 23:23:51 +0300

    help buttons work now

--- zoo-hope/src/components/PetPage/HelpPetBlock.tsx
+++ zoo-hope/src/components/PetPage/HelpPetBlock.tsx
@@ -1,8 +1,14 @@
 import { useTranslation } from "react-i18next";
+import PopupWindow from "./popupWindow";
+import { PopupWarning } from "./PopupWarning";
 import "../../i18n/i18n";
+import { useState } from "react";
+
 
 export function HelpPetBlock() {
   const { t } = useTranslation();
+  const [modalActive, setModalActive] = useState<boolean>(false);
+  const [warningActive, setWarningActive] = useState<boolean>(false);
 
   return (
     <div className="helpPetBlock">
@@ -11,21 +17,27 @@ export function HelpPetBlock() {
         <p>{t('petPage_additional_help')}</p>
       </div>
       <div className="helpPetBlock__info">
-        <button className="helpPetBlock__info__topLeft helpPetBlock__info__button">
+        <button onClick={() => { setWarningActive(true);}}  className="helpPetBlock__info__topLeft helpPetBlock__info__button">
           <p>{t('pay_for_sterilization')}</p>
         </button>
-        <button className="helpPetBlock__info__topRight helpPetBlock__info__button">
+
+        <button onClick={() => { setWarningActive(true);}} className="helpPetBlock__info__topRight helpPetBlock__info__button">
           <p>{t('pay_for_therapy')}</p>
         </button>
-        <button className="helpPetBlock__info__downRight helpPetBlock__info__button">
+
+        <button onClick={() => { setWarningActive(true) }} className="helpPetBlock__info__downRight helpPetBlock__info__button">
           <p>{t('buy_toy')}</p>
         </button>
-        <button className="helpPetBlock__info__downLeft helpPetBlock__info__button">
+
+        <button className="helpPetBlock__info__downLeft helpPetBlock__info__button" onClick={() => { setModalActive(true) }}>
           <p>{t('temporary_detention')}</p>
         </button>
-        <div className="helpPetBlock__info__logo">
-        </div>
+
+        <div className="helpPetBlock__info__logo"></div>
       </div>
+
+      <PopupWindow active={modalActive} setActive={setModalActive} />
+      <PopupWarning warningActive={warningActive} setWarningActive={setWarningActive} />
     </div>
   );
 }
